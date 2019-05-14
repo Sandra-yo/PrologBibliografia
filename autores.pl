157498493d6a2f9d89283139e7d7d5922719ddd2
Sandra Valadez <sandra.valadez.dl@gmail.com>
2019-05-14 12:39:58 -0500

    first commit

---
+++ autores.pl
@@ -0,0 +1,21 @@
+autor(susan_collins).
+autor(andres_Oppenheimer).
+autor(isaac_Asimov).
+autor(stephen_king).
+autor(george_R_R_Martin).
+autor(cj_daugherty).
+
+libro(adonde_no_conozco_nada).
+libro(ulises2300).
+libro(it).
+libro(the_hunger_games).
+libro(sinsajo)
+libro(salvese_quien_pueda).
+libro(the_bicentenal_man).
+libro(cancion_de_fuego).
+libro(cimmeria).
+
+
+autorDe(antonio_malpica, adonde_no_conozco_nada).
+autorDe(antonio_malpica, ulises2300).
+autorDe(stephen_king,it).
