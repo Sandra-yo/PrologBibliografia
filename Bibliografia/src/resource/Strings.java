eb2529001f24715624f179835152bd1f33ab5b64
ArgelOrtiz <7624000argel>
2019-05-15 22:33:50 -0500

    display updated

--- Bibliografia/src/resource/Strings.java
+++ Bibliografia/src/resource/Strings.java
@@ -9,8 +9,20 @@ package resource;
  */
 public class Strings {
     
-    public static final String TITLE            = "Bliblioteca Virtual" ;
-    public static final String SUB_TITLE        = "Menú";
-    public static final String OK_BUTTON        = "Aceptar";//temporal
-    public static final String RESOURCE_ROUTE   = "src/resource/";
+    public static final String TITLE                    = "Bliblioteca Virtual" ;
+    public static final String SUB_TITLE                = "Menú";
+    public static final String OK_BUTTON                = "Aceptar";//temporal
+    public static final String RESOURCE_ROUTE           = "src/resource/";
+    public static final String MENU_TITLE_FIRST         = "Mostrar categorias";
+    public static final String MENU_TITLE_SECOND        = "";
+    public static final String MENU_TITLES_BUTTON       = "Buscar titulos";
+    public static final String MENU_GENDERS_BUTTON      = "Buscar generos";
+    public static final String MENU_EDITORIALS_BUTTON   = "Buscar editoriales";
+    public static final String MENU_WRITERS_BUTTON      = "Buscar escritores";
+    public static final String MENU_YEARS_BUTTON        = "Buscar años";
+    public static final String MENU_TITLE_BUTTON        = "Buscar por titulo";
+    public static final String MENU_GENDER_BUTTON       = "Buscar por genero";
+    public static final String MENU_EDITORIAL_BUTTON    = "Buscar por editorial";
+    public static final String MENU_WRITER_BUTTON       = "Buscar por escritor";
+    public static final String MENU_YEAR_BUTTON         = "Buscar por año";
 }
--- Bibliografia/src/view/Display.java
+++ Bibliografia/src/view/Display.java
@@ -1,6 +1,7 @@
 
 package view;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
@@ -15,12 +16,29 @@ import resource.Strings;
  * @author 
  *  Argel Ortiz Arenas
  *  15/05/2019
+ * 
+ *  Construction of graphical interface for 
+ *  the query system in a database of facts built in prolog
  */
 public final class Display{
     private JFrame screen; 
     private JLabel subTitle;
-    private JButton ok; //temp
+    private JLabel menuTitle;
     private JLabel cover;
+    
+    private JButton searchTitles;
+    private JButton searchGenders;
+    private JButton searchEditorials;
+    private JButton searchWriters;
+    private JButton searchYears;
+    
+    private JButton searchByTitle;
+    private JButton searchByGenders;
+    private JButton searchByEditorials;
+    private JButton searchByWriters;
+    private JButton searchByYear;
+    
+    private JButton more;
     
     public Display(){
         config();
@@ -32,7 +50,7 @@ public final class Display{
         screen = new JFrame();
         
         screen.setTitle(Strings.TITLE);
-        screen.setSize(400,400);
+        screen.setSize(400,450);
         screen.setLocationRelativeTo(null);
         screen.setLayout(null);
         screen.setResizable(false);
@@ -40,32 +58,122 @@ public final class Display{
     }
     
     protected void init(){
-        subTitle    = new JLabel();
-        ok          = new JButton();
-        cover       = new JLabel();
+        subTitle            = new JLabel();
+        menuTitle           = new JLabel();
+        cover               = new JLabel();
+        
+        searchTitles        = new JButton();
+        searchGenders       = new JButton();
+        searchEditorials    = new JButton();
+        searchWriters       = new JButton();
+        searchYears         = new JButton();
+        
+        searchByTitle       = new JButton();
+        searchByGenders     = new JButton();
+        searchByEditorials  = new JButton();
+        searchByWriters     = new JButton();
+        searchByYear        = new JButton();
+        
+        more                = new JButton();
         
         //component config
         subTitle.setText(Strings.SUB_TITLE);
         subTitle.setBounds(175, 10, 50, 20);
         
-        ok.setText(Strings.OK_BUTTON);
-        ok.setBounds(125, 350, 150, 20);
-        
         cover.setIcon(new ImageIcon(Strings.RESOURCE_ROUTE+"cover.png"));
-        cover.setBounds(10, 25, 200, 200);
+        cover.setBounds(50, 25, 300, 200);
         
+        menuTitle.setText(Strings.MENU_TITLE_FIRST);
+        menuTitle.setBounds(25,225,200,20);
+        
+        searchTitles.setText(Strings.MENU_TITLES_BUTTON);
+        searchTitles.setBounds(25, 260, 150, 20);
+        
+        searchGenders.setText(Strings.MENU_GENDERS_BUTTON);
+        searchGenders.setBounds(25, 290, 150, 20);
+        
+        searchEditorials.setText(Strings.MENU_EDITORIALS_BUTTON);
+        searchEditorials.setBounds(25, 320, 150, 20);
+        
+        searchWriters.setText(Strings.MENU_WRITERS_BUTTON);
+        searchWriters.setBounds(25, 350, 150, 20);
+        
+        searchYears.setText(Strings.MENU_YEARS_BUTTON);
+        searchYears.setBounds(25, 380, 150, 20);
+        
+        searchByTitle.setText(Strings.MENU_TITLE_BUTTON);
+        searchByTitle.setBounds(200, 260, 150, 20);
+        
+        searchByGenders.setText(Strings.MENU_GENDER_BUTTON);
+        searchByGenders.setBounds(200, 290, 150, 20);
+        
+        searchByEditorials.setText(Strings.MENU_EDITORIAL_BUTTON);
+        searchByEditorials.setBounds(200, 320, 150, 20);
+        
+        searchByWriters.setText(Strings.MENU_WRITER_BUTTON);
+        searchByWriters.setBounds(200, 350, 150, 20);
+        
+        searchByYear.setText(Strings.MENU_YEAR_BUTTON);
+        searchByYear.setBounds(200, 380, 150, 20);
         
         listeners();
         
         screen.add(subTitle);
-        screen.add(ok);
         screen.add(cover);
+        screen.add(menuTitle);
+        screen.add(searchTitles);
+        screen.add(searchGenders);
+        screen.add(searchEditorials);
+        screen.add(searchWriters);
+        screen.add(searchYears);
+        screen.add(searchByTitle);
+        screen.add(searchByGenders);
+        screen.add(searchByEditorials);
+        screen.add(searchByWriters);
+        screen.add(searchByYear);
+        
                 
     }
     
     protected void listeners(){
         //ok button listener
-        ok.addActionListener((ActionEvent ae) -> {
+        searchTitles.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchGenders.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchEditorials.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchWriters.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchYears.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchByTitle.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchByGenders.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchByEditorials.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchByWriters.addActionListener((ActionEvent ae) -> {
+            System.out.println("click");
+        });
+        
+        searchByYear.addActionListener((ActionEvent ae) -> {
             System.out.println("click");
         });
     }
