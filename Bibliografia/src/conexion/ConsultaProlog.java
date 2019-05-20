57dbdf5aeab163da96c75e0ede558c5b04ce94b5
Sandra Valadez <sandra.valadez.dl@gmail.com>
2019-05-19 19:19:53 -0500

    interfazProlog

---
+++ Bibliografia/src/conexion/ConsultaProlog.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import java.awt.HeadlessException;
+import javax.swing.JOptionPane;
+import org.jpl7.Query;
+
+
+
+/**
+ *
+ * @author 52477
+ */
+public class ConsultaProlog {
+   
+    public static String run(String busqueda){
+        try{
+           Query q = new Query("consult('src/database/autores.pl')");
+            if(q.hasSolution()){
+                System.out.println("Conectando....");
+                //String personaje=JOptionPane.showInputDialog("Personaje:");
+             Query q2 = new Query(busqueda);
+             String answer="<html>";
+                while (q2.hasNext()) {
+                    answer+=q2.nextSolution().get("X")+"<br>";
+                }
+                answer+="</html>";
+                return(answer);
+            }else{
+                System.out.println("Imposible conectar");
+                return null;
+            }
+            
+        }catch(HeadlessException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ Bibliografia/src/view/TitlesSearch.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import conexion.ConsultaProlog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import resource.Strings;
+
+/**
+ *
+ * @author 52477
+ */
+public class TitlesSearch {
+    
+    private JFrame screen; 
+    private JLabel search;
+    private JLabel type;
+    private JLabel result;
+    private JTextField titleToSearch;
+    private JButton searchButton;
+    private String consulta;
+    private boolean busqueda;
+    
+    public TitlesSearch(boolean busqueda, String consulta){
+        config();
+        init();
+        this.consulta = consulta;
+        this.busqueda = busqueda;
+        screen.setVisible(true);
+        screen.validate();
+    }
+    
+    protected void config(){
+        screen = new JFrame();
+        
+        screen.setTitle(Strings.TITLE);
+        screen.setSize(430,450);
+        screen.setLocationRelativeTo(null);
+        screen.setLayout(null);
+        screen.setResizable(false);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+     
+    }
+    
+    protected void init(){
+        this.search        = new JLabel("Introduzca el titulo"); 
+        this.type          = new JLabel("Busqueda por titulo");
+        this.titleToSearch = new JTextField(12);
+        this.result        = new JLabel("Aqui se pondra el resultado");
+        this.searchButton  = new JButton("Buscar");
+        
+        type.setBounds(140, -80, 150, 200);
+        search.setBounds(20, -100, 200, 300);
+        titleToSearch.setBounds(135, 35, 150, 30);
+        result.setBounds(20, -50, 500, 500);
+        searchButton.setBounds(290, 34, 100, 30);
+        screen.add(type);
+        screen.add(result);
+        if(busqueda){
+        screen.add(titleToSearch);
+        screen.add(search);
+        }
+        screen.add(searchButton);
+      
+        listener();
+    } 
+    
+    protected String searchResponse(){
+        return ConsultaProlog.run(consulta);
+    }
+    protected void listener(){
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                result.setText(searchResponse());
+                System.out.println(searchResponse());
+           }
+        });
+    }
+    public static void main(String[] args) {
+        //TitlesSearch n = new TitlesSearch("");
+    }
+    
+    
+}
